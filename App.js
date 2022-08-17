64da441226c0f3779a402f7d48e73a7acbc8834b
shakilmahammud <hshakil987@gmail.com>
2022-08-17 23:56:19 +0600

    audio app

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React from 'react';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import {useFonts} from 'expo-font';
+import FlashMessage from 'react-native-flash-message';
+import {Provider} from 'react-redux';
+import store from './store';
+import Navigation from './navigation';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
+
+let persistor = persistStore(store);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    let [fontsLoaded] = useFonts({
+        'Manrope-Bold': require('./assets/fonts/Manrope-Bold.ttf'),
+        'Manrope-Regular': require('./assets/fonts/Manrope-Regular.ttf'),
+        'Manrope-Medium': require('./assets/fonts/Manrope-Medium.ttf'),
+    });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    if (!fontsLoaded) {
+        return null;
+    } else {
+        return (
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <SafeAreaProvider>
+                        <Navigation />
+                        <StatusBar />
+                        <FlashMessage
+                            position="top"
+                            floating
+                            statusBarHeight={30}
+                        />
+                    </SafeAreaProvider>
+                </PersistGate>
+            </Provider>
+        );
+    }
+}
